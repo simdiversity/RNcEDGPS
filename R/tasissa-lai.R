e9f95740b9093be2af2acf1b83ee77871ef40d85
Mattia Egloff <mattia.egloff@unil.ch>
2020-05-12 06:59:36 +0200

    Adding Tasissa-Lai

--- R/tasissa-lai.R
+++ R/tasissa-lai.R
@@ -1,41 +1,45 @@
 #' importFrom BB BBsolve
+#' importFrom stats runif
 NULL
-# # CONVERTED FROM:
-# # Abiy Tasissa and Rongjie Lai, "Exact Reconstruction of Euclidean Distance Geometry Problem Using Low-rank Matrix Completion," in IEEE Transactions on Information Theory, 2018. doi: 10.1109/TIT.2018.2881749
-# # https://github.com/abiy-tasissa/Nonconvex-Euclidean-Distance-Geometry-Problem-Solver
-# #
-# # TODO: needs testing!
-#
-# # ORIGINAL NOTE:
-# # --------------------------------------------------------------------------
-# # This script is a numerical solution of the Euclidean distance geometry
-# # problem (EDG). Formally, consider a set of n points where partial
-# # inter-point distance information is provided. The goal of EDG is to find
-# # the coordinate of the points given this partial information.
-# # --------------------------------------------------------------------------
-# # The n points usually lie in a low dimensional space of size r << n, low
-# # rank. With this, the EDG problem can be set as low-rank completion problem
-# # which can be solved via nuclear norm minimization. We recover the Gram
-# # matrix, the inner product matrix, and follow classical MDS to recover the
-# # coordinates. For details, see the associated paper below.
-# # --------------------------------------------------------------------------
-# # Tasissa, Abiy, and Rongjie Lai."Exact Reconstruction of Euclidean Distance
-# # Geometry Problem Using Low-rank Matrix Completion." arXiv preprint
-# # arXiv:1804.04310 (2018).
-# # --------------------------------------------------------------------------
-# # The gram matrix is psd so nuclear norm minimization equates to trace.
-# # Our algorithm uses the Augmented Lagrangian framework to find the Gram
-# # matrix.
-# # --------------------------------------------------------------------------
-# # We assume the partial inter-distance information comes from a uniform
-# # random sample. The information is assumed to be exact.
-# # Dist is the full distance matrix. Weight is a binary matrix informing
-# # whether a given entry of Dist is chosen or not.
-# # --------------------------------------------------------------------------
-# # Rongjie Lai, Abiy Tasissa
-# # --------------------------------------------------------------------------
-#
-alternating_completion <- function(Dist,Selected,opts,lsopts) {
+
+#' Tasissa-Lai Algorithm
+#' CONVERTED FROM:
+#' Abiy Tasissa and Rongjie Lai, "Exact Reconstruction of Euclidean Distance Geometry Problem Using Low-rank Matrix Completion," in IEEE Transactions on Information Theory, 2018. doi: 10.1109/TIT.2018.2881749
+#' https://github.com/abiy-tasissa/Nonconvex-Euclidean-Distance-Geometry-Problem-Solver
+#'
+#' TODO: needs testing!
+
+#' @note:
+#' Original Note
+#' --------------------------------------------------------------------------
+#' This script is a numerical solution of the Euclidean distance geometry
+#' problem (EDG). Formally, consider a set of n points where partial
+#' inter-point distance information is provided. The goal of EDG is to find
+#' the coordinate of the points given this partial information.
+#' --------------------------------------------------------------------------
+#' The n points usually lie in a low dimensional space of size r << n, low
+#' rank. With this, the EDG problem can be set as low-rank completion problem
+#' which can be solved via nuclear norm minimization. We recover the Gram
+#' matrix, the inner product matrix, and follow classical MDS to recover the
+#' coordinates. For details, see the associated paper below.
+#' --------------------------------------------------------------------------
+#' Tasissa, Abiy, and Rongjie Lai."Exact Reconstruction of Euclidean Distance
+#' Geometry Problem Using Low-rank Matrix Completion." arXiv preprint
+#' arXiv:1804.04310 (2018).
+#' --------------------------------------------------------------------------
+#' The gram matrix is psd so nuclear norm minimization equates to trace.
+#' Our algorithm uses the Augmented Lagrangian framework to find the Gram
+#' matrix.
+#' --------------------------------------------------------------------------
+#' We assume the partial inter-distance information comes from a uniform
+#' random sample. The information is assumed to be exact.
+#' Dist is the full distance matrix. Weight is a binary matrix informing
+#' whether a given entry of Dist is chosen or not.
+#' --------------------------------------------------------------------------
+#' Rongjie Lai, Abiy Tasissa
+#' --------------------------------------------------------------------------
+
+alternating_completion <- function(D, Selected, opts, lsopts) {
   norm <- function(x) sqrt(sum(x^2))
   # -------------------------------------------------------------------------
   # Constructs the operator A which captures the linear operator:
@@ -71,10 +75,10 @@ alternating_completion <- function(Dist,Selected,opts,lsopts) {
   r <- opts$r;
   Rk <- opts$rank;
   # n <- number of points
-  # Dist <- D^{2}(i,j) <- {d_{i,j}^{2}} matrix
   n <- nrow(D)
+  D <- D^2
   # calculate the ground truth of inner-product matrix
-  IPM_Truth <- Dist - rowMeans(Dist)*diag(1, n);
+  IPM_Truth <- D - rowMeans(D)*diag(1, n);
   IPM_Truth <- -.5 * (IPM_Truth - array(1, c(n,1)) * colMeans(IPM_Truth));
   # indices of the randomly chosen entries of D
   IJ <- which(Selected == 1);
@@ -83,13 +87,13 @@ alternating_completion <- function(Dist,Selected,opts,lsopts) {
   edgeind <- i + (j - 1)*n
   # the minimization problem has linear constraint R_{Omega}(X) <- R_{Omega)(M)
   # Represent this constraint as A(X) <- b where b <- R_{omega}(M)
-  M <- Dist(edgeind);
+  M <- D[edgeind];
   b <- M;
   # -------------------------------------------------------------------------
   # main algorithm
   # -------------------------------------------------------------------------
   # initialize P, lagrangian multiplier
-  P <- runif(n,Rk)
+  P <- stats::runif(n,Rk)
   R <- P
   D1 <- rep(length(b),1)
   # initialize energies
@@ -108,17 +112,17 @@ alternating_completion <- function(Dist,Selected,opts,lsopts) {
     tmperr <- A_operator(P, i, j) - b
     D1 <- D1 + tmperr
     # total energy
-    E(i) <- sum(rowSums(P*P)) + 0.5*r*norm(D1)^2
+    E[i] <- sum(rowSums(P*P)) + 0.5*r*norm(D1)^2
     if (opts$printenergy == 1) {
       sprintf('Iteration %d, TotalE <- %f\n',i,E(i))
     }
     # calculate the energy E1
-    E1(i) <- 0.5 * r * norm(tmperr)^2
+    E1[i] <- 0.5 * r * norm(tmperr)^2
     # stopping condition
     if (i > 2) {
       cre <- abs(E[i] - E[i - 1])/E[i]
     }
-    if (E1(i) < opts$tol && cre < opts$tol) {
+    if (E1[i] < opts$tol && cre < opts$tol) {
       break
     }
   }
