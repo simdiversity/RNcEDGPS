9ba84b653649b3321dc3111ac3f182efadb11e40
Mattia Egloff <mattia.egloff@unil.ch>
2020-05-19 09:24:12 +0200

    Added test for disputedness

--- tests/testthat/test-bavaud.R
+++ tests/testthat/test-bavaud.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("disputedness works", {
+  M <- matrix(c(1, 2, NA, NA, 4, 19, 0, NA, 0), nrow = 3)
+  f < -c(.5, .3, .2)
+  p <- ncol(M)
+  n <- nrow(M)
+  do <- c()
+  for (k in 1:p) {
+  NumK = 0L; DenK = 0L
+  for (i in 1:n) {
+    for (j in 1:n) {
+      if ((is.na(M[i,k]) == FALSE) & (is.na(M[j,k]) == FALSE)) {
+        NumK = NumK + f[i] * f[j] * abs(M[i,k] - M[j,k])
+        DenK = DenK + f[i] * f[j]}
+      }}
+  do[k] = NumK/DenK
+  }
+  expect(all.equal(do, disputedness(M,f)))
 })
